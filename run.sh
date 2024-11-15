608cb753056e866b9860e7e59ecbd405c35722ad
Nikita Bobko <git@bobko.xyz>
2024-11-15 19:43:26 +0100

    Better run.sh script

--- run.sh
+++ run.sh
@@ -3,11 +3,16 @@ set -e # Exit if one of commands exit with non-zero exit code
 set -u # Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’ as an error
 set -o pipefail # Any command failed in the pipe fails the whole pipe
 # set -x # Print shell commands as they are executed (or you can try -v which is less verbose)
-# cd "$(dirname "$0")"
+cd "$(dirname "$0")"
 
 for dir in java kotlin; do
     cd $dir
         mvn clean verify
+    cd - > /dev/null
+done
+
+for dir in java kotlin; do
+    cd $dir
         java -jar target/benchmarks.jar 2>&1 | tee log
     cd - > /dev/null
 done
