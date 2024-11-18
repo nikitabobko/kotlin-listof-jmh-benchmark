152c4e576378e142f36c661afcb4fb397f805f81
Nikita Bobko <git@bobko.xyz>
2024-11-18 17:06:38 +0100

    const String -> Integer++ % 128

--- java/src/main/java/org/sample/JavaBenchmark.java
+++ java/src/main/java/org/sample/JavaBenchmark.java
@@ -1,112 +1,114 @@
 package org.sample;
 
-import kotlin.collections.CollectionsKt;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 
-import java.util.Arrays;
 import java.util.List;
 
 @State(Scope.Thread)
 @Fork(1) // todo drop fork for final testing
 public class JavaBenchmark {
-    String x = "foo";
+    int elem = 0;
+
+    private Integer elem() {
+        return Integer.valueOf(elem++ % 128);
+    }
 
     // @Benchmark
     // public List<String> kotlin_listOf_vararg_010() {
     //     return CollectionsKt.listOf(
-    //             x,
-    //             x,
-    //             x,
-    //             x,
-    //             x,
-    //             x,
-    //             x,
-    //             x,
-    //             x,
-    //             x
+    //             elem(),
+    //             elem(),
+    //             elem(),
+    //             elem(),
+    //             elem(),
+    //             elem(),
+    //             elem(),
+    //             elem(),
+    //             elem(),
+    //             elem()
     //     );
     // }
 
     @Benchmark
-    public List<String> java_util_List_of_010() {
+    public List<Integer> java_util_List_of_010() {
         return List.of(
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem()
         );
     }
 
     @Benchmark
-    public List<String> MyJavaListMimic_of_010() {
+    public List<Integer> MyJavaListMimic_of_010() {
         return MyJavaListMimic.of(
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem()
         );
     }
 
     @Benchmark
-    public List<String> MyJavaListMimic_ofVararg_010() {
+    public List<Integer> MyJavaListMimic_ofVararg_010() {
         return MyJavaListMimic.ofVararg(
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem()
         );
     }
 
     @Benchmark
-    public List<String> MyJavaListMimic_listFromTrustedArray_010() {
+    public List<Integer> MyJavaListMimic_listFromTrustedArray_010() {
         return MyJavaListMimic.listFromTrustedArray(
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x,
-                x
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem(),
+                elem()
         );
     }
 
     // @Benchmark
     // public List<String> Arrays_asList_010() {
     //     return Arrays.asList(
-    //             x,
-    //             x,
-    //             x,
-    //             x,
-    //             x,
-    //             x,
-    //             x,
-    //             x,
-    //             x,
-    //             x
+    //             elem(),
+    //             elem(),
+    //             elem(),
+    //             elem(),
+    //             elem(),
+    //             elem(),
+    //             elem(),
+    //             elem(),
+    //             elem(),
+    //             elem()
     //     );
     // }
 
