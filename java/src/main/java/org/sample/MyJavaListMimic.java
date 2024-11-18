13fd884655e209f94ac237d34aaf80d61848ee50
Nikita Bobko <git@bobko.xyz>
2024-11-18 15:46:33 +0100

    Cleanup

--- java/src/main/java/org/sample/MyJavaListMimic.java
+++ java/src/main/java/org/sample/MyJavaListMimic.java
@@ -9,18 +9,7 @@ import java.io.Serializable;
 import java.util.*;
 
 public class MyJavaListMimic {
-    public static <T> List<T> of(
-            T a01,
-            T a02,
-            T a03,
-            T a04,
-            T a05,
-            T a06,
-            T a07,
-            T a08,
-            T a09,
-            T a10
-    ) {
+    public static <T> List<T> of(T a01, T a02, T a03, T a04, T a05, T a06, T a07, T a08, T a09, T a10) {
         return MyJavaListMimic.listFromTrustedArray(a01, a02, a03, a04, a05, a06, a07, a08, a09, a10);
     }
 
